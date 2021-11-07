515426d74db284f0606d28db78d64ac498dc0f43
Allen K Abraham <allenabraham777@gmail.com>
2021-11-07 23:46:52 +0530

    typo fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Home = () => {
           <div className="home-heading-main">ALLEN&nbsp;&nbsp;K ABRAHAM</div>
           <h1 className="home-heading-one">Hey There,</h1>
           <h2 className="home-heading-two">Welcome to my space</h2>
-          <h3 className="home-heading-three">I'm just write some random code</h3>
+          <h3 className="home-heading-three">I just write some random code</h3>
           <a href="https://drive.google.com/file/d/19vuRZ7gWNhiNgaZPOuiKdr8xbvNhL0Xk/view?usp=sharing" className="home-heading-button">Download Resume <span className="download">▼</span></a>
         </div>
         <div className="home-image">
