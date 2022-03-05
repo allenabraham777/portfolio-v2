303c704d1c2f203d3b89887ca3e428990041567b
Allen K Abraham <allenabraham777@gmail.com>
2022-03-05 17:57:42 +0530

    Add social url

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsLinkedin, BsGithub } from 'react-icons/bs';
+import Seo from "../components/Seo";
 
 import Layout from "../layouts/default";
 import "../styles/pages/index.scss";
@@ -7,16 +8,17 @@ import "../styles/pages/index.scss";
 const Home = () => {
   return (
     <Layout fullHeight>
+      <Seo title="Home" />
       <div className="homepage">
         <div className="homepage__title">
           <h1> Hey There,</h1>
           <h3>Welcome to my space</h3>
         </div>
         <div className="homepage__social">
-          <a href="" className="linkedin">
+          <a href="https://linkedin.com/in/allenabraham777" className="linkedin">
             <BsLinkedin/>
           </a>
-          <a href="" className="github">
+          <a href="https://github.com/allenabraham777" className="github">
             <BsGithub/>
           </a>
         </div>
